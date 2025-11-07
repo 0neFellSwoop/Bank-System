cf195d417a65e59eeddfeac532ad56f0e2ee8c1c
Isaac Burstyn <irb36@drexel.edu>
2025-11-07 16:18:23 -0500

    Assignment 6: Created CommandStorage and CommandStorageTest

--- src/main/java/CommandProcessor.java
+++ src/main/java/CommandProcessor.java
@@ -10,17 +10,22 @@ public class CommandProcessor {
         String[] parsedCommand = command.split(" ");
         switch(parsedCommand[0]){
             case "create":
+                String ID = parsedCommand[2];
+                double APR = Double.parseDouble(parsedCommand[3]);
                 switch(parsedCommand[1]){
                     case "checking":
-                        BANK.addAccount(new CheckingAccount(parsedCommand[2], Double.parseDouble(parsedCommand[3])));
+                        BANK.addAccount(new CheckingAccount(ID, APR));
                         break;
                     case "savings":
+                        BANK.addAccount(new SavingsAccount(ID, APR));
                         break;
                     case "cd":
+                        BANK.addAccount(new CDAccount(ID, APR, Double.parseDouble(parsedCommand[4])));
                         break;
                 }
                 break;
             case "deposit":
+                BANK.deposit(parsedCommand[1], Double.parseDouble(parsedCommand[2]));
                 break;
         }
 
--- src/test/java/CommandProcessorTest.java
+++ src/test/java/CommandProcessorTest.java
@@ -8,6 +8,7 @@ public class CommandProcessorTest {
     Bank bank;
     CommandProcessor commandProcessor;
     String command;
+    Account account;
 
     @BeforeEach
     void setUp(){
@@ -21,8 +22,51 @@ public class CommandProcessorTest {
         commandProcessor.process(command);
         assertEquals("12345678", bank.retrieveAccount("12345678").getID());
         assertEquals(0.1, bank.retrieveAccount("12345678").getAPR());
-
     }
 
+    @Test
+    void create_savings_account(){
+        command = "create savings 12345678 4.1";
+        commandProcessor.process(command);
+        assertEquals("12345678", bank.retrieveAccount("12345678").getID());
+        assertEquals(4.1, bank.retrieveAccount("12345678").getAPR());
+    }
+
+    @Test
+    void create_cd_account(){
+        command = "create cd 12345678 9.7 2000";
+        commandProcessor.process(command);
+        assertEquals("12345678", bank.retrieveAccount("12345678").getID());
+        assertEquals(9.7, bank.retrieveAccount("12345678").getAPR());
+        assertEquals(2000, bank.retrieveAccount("12345678").getBalance());
+    }
+
+    @Test
+    void deposit_into_checking_account(){
+        account = new CheckingAccount("12345678", .6);
+        bank.addAccount(account);
+        command = "deposit 12345678 500";
+        commandProcessor.process(command);
+        assertEquals(500, bank.retrieveAccount("12345678").getBalance());
+    }
+
+    @Test
+    void deposit_into_savings_account(){
+        account = new SavingsAccount("12345678", 5);
+        bank.addAccount(account);
+        command = "deposit 12345678 1500";
+        commandProcessor.process(command);
+        assertEquals(1500, bank.retrieveAccount("12345678").getBalance());
+    }
+
+    @Test
+    void deposit_several_times_works_as_expected(){
+        account = new SavingsAccount("12345678", 5);
+        bank.addAccount(account);
+        command = "deposit 12345678 1500";
+        commandProcessor.process(command);
+        commandProcessor.process(command);
+        assertEquals(3000, bank.retrieveAccount("12345678").getBalance());
+    }
 
 }
