72641e3cc8f2c94b4e0365247f357e83de95ecd9
Isaac Burstyn <irb36@drexel.edu>
2025-12-02 14:08:01 -0500

    PassTimeCommandValidator: Complete

--- src/main/java/banking/PassTimeCommandValidator.java
+++ src/main/java/banking/PassTimeCommandValidator.java
@@ -3,7 +3,17 @@ package banking;
 public class PassTimeCommandValidator {
 
 
-    public boolean validate(String[] command, Bank bank) {
-
+    public boolean validate(String[] command) {
+        if(command.length != 2){
+            return false;
+        } else {
+            int months;
+            try {
+                months = Integer.parseInt(command[1]);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            return months > 0 && months <= 60;
+        }
     }
 }
