5e113bff82cb706dd75602ac9523e86de92aace6
Isaac Burstyn <irb36@drexel.edu>
2025-12-04 13:53:06 -0500

    CheckingAccount: Interest Refactor

--- src/main/java/banking/CheckingAccount.java
+++ src/main/java/banking/CheckingAccount.java
@@ -7,13 +7,6 @@ public class CheckingAccount extends Account{
     }
 
     @Override
-    public void accrueInterest(int months) {
-        for(int i = 0; i < months; i++){
-            super.deposit(super.getBalance() * super.getAPR()/100/12);
-        }
-    }
-
-    @Override
     public boolean validateDeposit(double amount) {
         return !(amount > 1000);
     }
