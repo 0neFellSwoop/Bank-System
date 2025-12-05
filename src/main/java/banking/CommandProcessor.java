b4bde9d8302e52f0e89279c0608c8947de10dca7
Isaac Burstyn <irb36@drexel.edu>
2025-12-05 10:32:58 -0500

    Code Quality: Processor Refactor, unused file deletion

--- src/main/java/banking/CommandProcessor.java
+++ src/main/java/banking/CommandProcessor.java
@@ -11,25 +11,55 @@ public class CommandProcessor {
         this.BANK = bank;
     }
 
+    private void processCreateCommand(String[] parsedCommand){
+        String ID = parsedCommand[2];
+        double APR = Double.parseDouble(parsedCommand[3]);
+        switch(parsedCommand[1].toLowerCase()){
+            case "checking":
+                BANK.addAccount(new CheckingAccount(ID, APR));
+                break;
+            case "savings":
+                BANK.addAccount(new SavingsAccount(ID, APR));
+                break;
+            case "cd":
+                BANK.addAccount(new CDAccount(ID, APR, Double.parseDouble(parsedCommand[4])));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void processTransferCommand(String[] parsedCommand){
+        double amount = Double.parseDouble(parsedCommand[3]);
+        String senderID = parsedCommand[1];
+        String destinationID = parsedCommand[2];
+        double senderBalance = BANK.retrieveAccount(senderID).getBalance();
+        BANK.withdraw(senderID, amount);
+        if(amount > senderBalance){
+            amount = senderBalance;
+        }
+        BANK.deposit(destinationID, amount);
+    }
+
+    private void processPassTimeCommand(String[] parsedCommand) {
+        Iterator<Map.Entry<String, Account>> iterator = BANK.getAccounts().entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<String, Account> account = iterator.next();
+            double balance = account.getValue().getBalance();
+            if(balance == 0){
+                iterator.remove();
+            } else if (balance < 100) {
+                BANK.withdraw(account.getValue().getID(), 25);
+            }
+            account.getValue().accrueInterest(Integer.parseInt(parsedCommand[1]));
+        }
+    }
+
     public void process(String command) {
         String[] parsedCommand = command.split(" ");
         switch(parsedCommand[0].toLowerCase()){
             case "create":
-                String ID = parsedCommand[2];
-                double APR = Double.parseDouble(parsedCommand[3]);
-                switch(parsedCommand[1].toLowerCase()){
-                    case "checking":
-                        BANK.addAccount(new CheckingAccount(ID, APR));
-                        break;
-                    case "savings":
-                        BANK.addAccount(new SavingsAccount(ID, APR));
-                        break;
-                    case "cd":
-                        BANK.addAccount(new CDAccount(ID, APR, Double.parseDouble(parsedCommand[4])));
-                        break;
-                    default:
-                        break;
-                }
+                processCreateCommand(parsedCommand);
                 break;
             case "deposit":
                 BANK.deposit(parsedCommand[1], Double.parseDouble(parsedCommand[2]));
@@ -38,32 +68,13 @@ public class CommandProcessor {
                 BANK.withdraw(parsedCommand[1], Double.parseDouble(parsedCommand[2]));
                 break;
             case "transfer":
-                double amount = Double.parseDouble(parsedCommand[3]);
-                String senderID = parsedCommand[1];
-                String destinationID = parsedCommand[2];
-                double senderBalance = BANK.retrieveAccount(senderID).getBalance();
-                BANK.withdraw(senderID, amount);
-                if(amount > senderBalance){
-                    amount = senderBalance;
-                }
-                BANK.deposit(destinationID, amount);
+                processTransferCommand(parsedCommand);
                 break;
             case "pass":
-                Iterator<Map.Entry<String, Account>> iterator = BANK.getAccounts().entrySet().iterator();
-                while(iterator.hasNext()){
-                    Map.Entry<String, Account> account = iterator.next();
-                    double balance = account.getValue().getBalance();
-                    if(balance == 0){
-                        iterator.remove();
-                    } else if (balance < 100) {
-                        BANK.withdraw(account.getValue().getID(), 25);
-                    }
-                    account.getValue().accrueInterest(Integer.parseInt(parsedCommand[1]));
-                }
+                processPassTimeCommand(parsedCommand);
+                break;
             default:
                 break;
-
-
         }
 
     }
